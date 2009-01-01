628956f52be8d9ad1e4908f7a8555969758a3f95
dpvc <>
2009-01-01 03:10:30 +0000

    Updated to v3.6b

--- jsMath-version-check.js
+++ jsMath-version-check.js
@@ -26,7 +26,7 @@
 /********************************************************************/
 
 jsMath.Controls.TestVersion = function () {
-  var version = 'v3.6a';
+  var version = 'v3.6b';
   var download = 'http://www.math.union.edu/locate/jsMath/download/jsMath.html';
 
   jsMath.Controls.panel.style.display = 'none';
