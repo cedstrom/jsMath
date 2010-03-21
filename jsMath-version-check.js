53d3644bdd87d2e5de797e2f78cd10e4cbf6480b
dpvc <>
2010-03-21 01:33:04 +0000

    Bring jsMath up to version 3.6e.  Changes include:
    * Changed the way IE is detected to be more robust for IE8 in its
      various modes.
    
    * More fixes for vertical and horizontal alignment in IE8 in all its
      various modes, and in jsMath's various disoplay modes.  More
      thurough testing than with 3.6d, which introduced some problems in
      some modes.
    
    * Add <code> to the list of node types that are not scanned for
      mathematics by the tex2math plugin.
    
    * Updated the unicode fallback mode font tables for Mac and PC to
      handle current font configurations better.

--- jsMath-version-check.js
+++ jsMath-version-check.js
@@ -26,7 +26,7 @@
 /********************************************************************/
 
 jsMath.Controls.TestVersion = function () {
-  var version = 'v3.6d';
+  var version = 'v3.6e';
   var download = 'http://www.math.union.edu/locate/jsMath/download/jsMath.html';
 
   jsMath.Controls.panel.style.display = 'none';
